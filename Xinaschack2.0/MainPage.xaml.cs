0d1a61f8cd3ea69afc1c2b45a7567239ed6594b7
Elmabt <elmabristric20@gmail.com>
2021-09-17 10:30:19 +0200

    first commit

--- Xinaschack2.0/MainPage.xaml.cs
+++ Xinaschack2.0/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,26 +25,90 @@ namespace Xinaschack2._0
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+
+
     public sealed partial class MainPage : Page
     {
+        private CanvasBitmap StartScreen { get; set; }
+        private CanvasBitmap Board { get; set; }
+        private readonly float DesignWidth = 1280;
+        private readonly float DesignHeight = 720;
+        private List<Rect> RectList { get; set; }
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(720, 1280);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
         }
 
-        private void GameCanvas_CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+
+        private async Task CreateResourcesAsync(CanvasAnimatedControl sender)
+        {
+            Board = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/spelplan2.png"));
+
+
+        }
+
+        private void BackgroundCanvas_createResources(CanvasControl sender, CanvasCreateResourcesEventArgs args)
         {
 
         }
 
-        private void GameCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args)
+
+        private void GameCanvas_CreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
+        {
+            args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
+        }
+
+        private void GameCanvas_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
+            //args.DrawingSession.DrawImage(StartScreen);
+            args.DrawingSession.DrawImage(Board);
+
+        }
+
+        private void GameCanvas_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
         {
 
         }
 
-        private void GameCanvas_Update(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedUpdateEventArgs args)
+        private void MakeRectList()
         {
-
+            // ball size = 34x34
+            // middle of 1280 = 640
+            // bottom of board = 632
+            // top of board = 67
+            // Point middle_bottom_point = new Point(640, )
+            // RectList.Add(new Rect(new Point(6), new Size()));
         }
+
+
+
+        // private void GameCanvas_CreateResources2(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs args)
+        //{
+
+        //  args.TrackAsyncAction(CreateResourcesAsync2(sender).AsAsyncAction());
+
+        //        }
+        //private async Task CreateResourcesAsync2(CanvasAnimatedControl sender)
+        //{
+        //  Board = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/spelplan2.png"));
+
+        //}
+
+        //      private void GameCanvas_Draw2(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        //    {
+
+        //  }
+
+        // private void GameCanvas_Update2(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
+        //{
+
+        //        }
     }
 }
+
